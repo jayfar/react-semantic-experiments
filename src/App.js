95f09f871423bc7b6d8da30eb2709b457ded6ab0
Jason <jayfar@yahoo.com>
2017-05-12 14:25:45 -0400

    Added a few additional semantic components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import Test1 from './components/Test1.js';
+import ModalSimple from './components/ModalSimple.js';
+
+import { Menu } from 'semantic-ui-react'
 
 class App extends Component {
+
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     return (
       <div>
+
+        <Menu fixed="top">
+            <Menu.Item header>Our Company</Menu.Item>
+            <Menu.Item
+              name='editorials'
+              active={activeItem === 'editorials'}
+              onClick={this.handleItemClick}
+            >
+              Editorials
+            </Menu.Item>
+
+            <Menu.Item
+              name='reviews'
+              active={activeItem === 'reviews'}
+              onClick={this.handleItemClick}
+            >
+              Reviews
+            </Menu.Item>
+
+            <Menu.Item
+              name='upcomingEvents'
+              active={activeItem === 'upcomingEvents'}
+              onClick={this.handleItemClick}
+            >
+              Upcoming Events
+            </Menu.Item>
+        </Menu>
+
+        <div style={{height:"50"}}/>
+
 		<Test1/>
+        <hr/>
+        <ModalSimple />
+        
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
       </div>
     );
   }
---
+++ src/components/ModalSimple.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+
+class ModalSimple extends Component {
+  state = { open: false }
+
+  open = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
+
+  onBtnShowModalClick = () => this.open();
+
+  render() {
+    const { open } = this.state
+
+    return (
+        <div>
+          <Modal
+            dimmer={true}
+            open={open}
+            onOpen={this.open}
+            onClose={this.close}
+            size='small'
+          >
+            <Modal.Header>Simple Modal Test</Modal.Header>
+            <Modal.Content>
+              <p>My Modal Content</p>
+            </Modal.Content>
+            <Modal.Actions>
+
+              <Button icon='remove' content='Cancel' onClick={this.close} />
+              <Button icon='check' content='All Done' onClick={this.close} />
+            </Modal.Actions>
+          </Modal>
+
+          <Button onClick={this.onBtnShowModalClick}> Test My Modal</Button>
+          
+          </div>
+    )
+  }
+}
+
+export default ModalSimple
